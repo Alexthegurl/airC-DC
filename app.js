a3259ed6557e5faefb90844d0224731bc218a61f
originaloregano <aorigitano@gmail.com>
2017-12-01 14:29:06 -0600

    styling updates

--- app.js
+++ app.js
@@ -37,16 +37,16 @@ app.use('/favicon.ico', express.static('favicon.ico'));
 app.use('/', index);
 app.use('/users', users);
 
-app.get('/drums', function(req, res) { res.render('drums')})
-app.get('/team', function(req, res) { res.render('team')})
-app.get('/piano', function(req, res) { res.render('piano')})
-app.get('/bass', function(req, res) { res.render('bass')})
-app.get('/trump', function(req, res) { res.render('trump')})
-app.get('/dj1', function(req, res) { res.render('dj1')})
-app.get('/dj2', function(req, res) { res.render('dj2')})
-app.get('/scenes', function(req, res) { res.render('scenes')})
-app.get('/guitar', function(req, res) { res.render('guitar')})
-app.get('/gong', function(req, res) { res.render('gong')})
+app.get('/drums', function(req, res) { res.render('drums');})
+app.get('/team', function(req, res) { res.render('team');})
+app.get('/piano', function(req, res) { res.render('piano');})
+app.get('/bass', function(req, res) { res.render('bass');})
+app.get('/trump', function(req, res) { res.render('trump');})
+app.get('/dj1', function(req, res) { res.render('dj1');})
+app.get('/dj2', function(req, res) { res.render('dj2');})
+app.get('/scenes', function(req, res) { res.render('scenes');})
+app.get('/guitar', function(req, res) { res.render('guitar');})
+app.get('/gong', function(req, res) { res.render('gong');})
 
 
 // catch 404 and forward to error handler
